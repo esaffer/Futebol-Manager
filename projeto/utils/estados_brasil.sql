edea83867329b54aa9ec7d490cb22d21e0b9d09f
Bruno Merino-Bailón <bruno@mandragora.cc>
2010-01-04 16:31:15 -0200

    Algumas alterações nos Bancos de Dados de Estados e Cidades.

--- projeto/utils/estados_brasil.sql
+++ projeto/utils/estados_brasil.sql
@@ -1,34 +1,34 @@
-CREATE TABLE "region" (
-	"id"			SMALLINT NOT NULL,
-	"country_id"	SMALLINT NOT NULL,
-	"short_name"	CHAR(2) NOT NULL,
-	"name"			VARCHAR(50) NOT NULL
+CREATE TABLE region (
+	id			SMALLINT NOT NULL,
+	country_id	SMALLINT NOT NULL,
+	short_name	CHAR(2) NOT NULL,
+	name		VARCHAR(50) NOT NULL
 );
 
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (01, 76, 'AC', 'Acre');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (02, 76, 'AL', 'Alagoas');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (03, 76, 'AP', 'Amapá');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (04, 76, 'AM', 'Amazonas');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (05, 76, 'BA', 'Bahia');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (06, 76, 'CE', 'Ceará');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (07, 76, 'DF', 'Distrito Federal');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (08, 76, 'ES', 'Espírito Santo');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (09, 76, 'GO', 'Goiás');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (10, 76, 'MA', 'Maranhão');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (11, 76, 'MT', 'Mato Grosso');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (12, 76, 'MS', 'Mato Grosso do Sul');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (13, 76, 'MG', 'Minas Gerais');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (14, 76, 'PA', 'Pará');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (15, 76, 'PB', 'Paraíba');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (16, 76, 'PR', 'Paraná');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (17, 76, 'PE', 'Pernambuco');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (18, 76, 'PI', 'Piauí');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (19, 76, 'RJ', 'Rio de Janeiro');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (20, 76, 'RN', 'Rio Grande do Norte');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (21, 76, 'RS', 'Rio Grande do Sul');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (22, 76, 'RO', 'Rondônia');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (23, 76, 'RR', 'Roraima');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (24, 76, 'SC', 'Santa Catarina');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (25, 76, 'SP', 'São Paulo');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (26, 76, 'SE', 'Sergipe');
-INSERT INTO "region" ("id", "country_id", "short_name", "name") VALUES (27, 76, 'TO', 'Tocantins');
+INSERT INTO region (id, country_id, short_name, name) VALUES (01, 76, 'AC', 'Acre');
+INSERT INTO region (id, country_id, short_name, name) VALUES (02, 76, 'AL', 'Alagoas');
+INSERT INTO region (id, country_id, short_name, name) VALUES (03, 76, 'AP', 'Amapá');
+INSERT INTO region (id, country_id, short_name, name) VALUES (04, 76, 'AM', 'Amazonas');
+INSERT INTO region (id, country_id, short_name, name) VALUES (05, 76, 'BA', 'Bahia');
+INSERT INTO region (id, country_id, short_name, name) VALUES (06, 76, 'CE', 'Ceará');
+INSERT INTO region (id, country_id, short_name, name) VALUES (07, 76, 'DF', 'Distrito Federal');
+INSERT INTO region (id, country_id, short_name, name) VALUES (08, 76, 'ES', 'Espírito Santo');
+INSERT INTO region (id, country_id, short_name, name) VALUES (09, 76, 'GO', 'Goiás');
+INSERT INTO region (id, country_id, short_name, name) VALUES (10, 76, 'MA', 'Maranhão');
+INSERT INTO region (id, country_id, short_name, name) VALUES (11, 76, 'MT', 'Mato Grosso');
+INSERT INTO region (id, country_id, short_name, name) VALUES (12, 76, 'MS', 'Mato Grosso do Sul');
+INSERT INTO region (id, country_id, short_name, name) VALUES (13, 76, 'MG', 'Minas Gerais');
+INSERT INTO region (id, country_id, short_name, name) VALUES (14, 76, 'PA', 'Pará');
+INSERT INTO region (id, country_id, short_name, name) VALUES (15, 76, 'PB', 'Paraíba');
+INSERT INTO region (id, country_id, short_name, name) VALUES (16, 76, 'PR', 'Paraná');
+INSERT INTO region (id, country_id, short_name, name) VALUES (17, 76, 'PE', 'Pernambuco');
+INSERT INTO region (id, country_id, short_name, name) VALUES (18, 76, 'PI', 'Piauí');
+INSERT INTO region (id, country_id, short_name, name) VALUES (19, 76, 'RJ', 'Rio de Janeiro');
+INSERT INTO region (id, country_id, short_name, name) VALUES (20, 76, 'RN', 'Rio Grande do Norte');
+INSERT INTO region (id, country_id, short_name, name) VALUES (21, 76, 'RS', 'Rio Grande do Sul');
+INSERT INTO region (id, country_id, short_name, name) VALUES (22, 76, 'RO', 'Rondônia');
+INSERT INTO region (id, country_id, short_name, name) VALUES (23, 76, 'RR', 'Roraima');
+INSERT INTO region (id, country_id, short_name, name) VALUES (24, 76, 'SC', 'Santa Catarina');
+INSERT INTO region (id, country_id, short_name, name) VALUES (25, 76, 'SP', 'São Paulo');
+INSERT INTO region (id, country_id, short_name, name) VALUES (26, 76, 'SE', 'Sergipe');
+INSERT INTO region (id, country_id, short_name, name) VALUES (27, 76, 'TO', 'Tocantins');
