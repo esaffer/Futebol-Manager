ef169ca461a03619be6a59e8d82f628b2ecbd61a
Bruno Merino-Bailón <bruno@chaos.(none)>
2010-01-19 20:56:50 -0200

    Arquivo com o código do Google Analytics.

---
+++ app/views/static/analytics.php
@@ -0,0 +1,17 @@
+<?
+/*****************************************************************************
+ * analytics.php
+ * Código JS para o tracking do Google Analytics.
+ * www.google.com/analytics
+ *****************************************************************************/
+?>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-12555405-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
