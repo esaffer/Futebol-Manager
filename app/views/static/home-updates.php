25f335dc73c7106048228af7d8b71c4030752275
Bruno Merino-Bailón <bruno@mandragora.cc>
2010-01-27 00:51:49 -0200

    Edições no design - 3.

--- app/views/static/home-updates.php
+++ app/views/static/home-updates.php
@@ -1,9 +1,9 @@
-<div id='updates'>";
-	<div id='update-title'>Notícias</div>
-	<div id='warning-content'>
+<div id='updates'>
+	<span class='update-title'>Notícias</span>
+	<span class='warning-content'>
 		<ul>
 			<li><span class='data'>2010-01-26</span>Alterações no design.</li>
 			<li><span class='data'>2010-01-20</span>O aplicativo entra no ar com a maioria das funcionalidades!</li>
 		</ul>
-	</div>
+	</span>
 </div>
